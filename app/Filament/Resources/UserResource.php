b1781d1f4746d91b85bde2d00c1b538bb4ba5230
Danilo Yui Honda <hondadanilo6@gmail.com>
2025-02-06 16:42:43 -0300

    melhorias no cadastro de usuario

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -2,40 +2,84 @@
 
 namespace App\Filament\Resources;
 
+use Altwaireb\World\Models\City;
+use Altwaireb\World\Models\State;
 use App\Filament\Resources\UserResource\Pages;
 use App\Models\User;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Illuminate\Support\Collection;
 
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
+    protected static ?string $navigationLabel = 'Employees';
+
     protected static ?string $navigationGroup = 'Creation';
 
     protected static ?int $navigationSort = 9;
 
-    protected static ?string $navigationIcon = 'heroicon-o-user-circle';
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                TextInput::make('name')
-                    ->required()
-                    ->maxLength(50),
-                TextInput::make('email')
-                    ->email()
-                    ->required()
-                    ->maxLength(50),
-                TextInput::make('password')
-                    ->password()
-                    ->required()
-                    ->maxLength(50),
+                Section::make('Personal Info')
+                    ->schema([
+                        TextInput::make('name')
+                            ->helperText(str('Your **full name** here')->markdown()->toHtmlString())
+                            ->required()
+                            ->maxLength(50),
+                        TextInput::make('email')
+                            ->email()
+                            ->required()
+                            ->maxLength(50),
+                        TextInput::make('password')
+                            ->password()
+                            ->required()
+                            ->maxLength(50),
+                    ])->columns(3),
+
+                Section::make('Address Info')
+                    ->schema([
+                        Select::make('country_id')
+                            ->relationship(name: 'country', titleAttribute: 'name')
+                            ->searchable()
+                            ->preload()
+                            ->live()
+                            ->required(),
+
+                        Select::make('state_id')
+                            ->label('State')
+                            ->options(fn (Get $get): Collection => State::query()
+                                ->where('country_id', $get('country_id'))
+                                ->pluck('name', 'id'))
+                            ->searchable()
+                            ->preload()
+                            ->required()
+                            ->disabled(fn (Get $get): bool => State::where('country_id', $get('country_id'))->doesntExist() // Desativa se não houver estados
+                            ),
+
+                        Select::make('city_id')
+                            ->label('City')
+                            ->options(fn (Get $get): Collection => City::query()
+                                        ->where('state_id', $get('state_id'))
+                                        ->pluck('name', 'id'))
+                            ->searchable()
+                            ->preload()
+                            ->required()
+                            ->disabled(fn (Get $get): bool => State::where('country_id', $get('country_id'))->doesntExist() // Desativa cidades se não houver estados
+                            ),
+                    ])->columns(3),
             ]);
     }
 
@@ -57,7 +101,7 @@ class UserResource extends Resource
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
---
+++ database/migrations/2025_02_06_181101_add_address_fields_to_users_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('country_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('state_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('city_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('adress')->nullable();
+            $table->string('postal_code')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['country_id', 'state_id', 'city_id', 'adress', 'postal_code']);
+        });
+    }
+};
+
